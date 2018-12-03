a3f0c3e815060593306f60ddcd03e2f2ec740496
NikitaM <nikita.milka@accenture.com>
2018-12-03 09:42:08 +0000

    N/A - Added test rule for logging test case name

--- src/test/java/lv/bvef/ui/RegistrationTests.java
+++ src/test/java/lv/bvef/ui/RegistrationTests.java
@@ -3,7 +3,11 @@ package lv.bvef.ui;
 import lv.bvef.ui.pageobjects.AuthenticationPage;
 import lv.bvef.ui.pageobjects.MainPage;
 import lv.bvef.ui.pageobjects.RegistrationPage;
+import org.apache.log4j.Logger;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.*;
+import org.junit.runner.Description;
 
 import static com.codeborne.selenide.Selenide.open;
 import static lv.bvef.ui.utils.RandomGenerator.*;
@@ -11,6 +15,15 @@ import static testHelpers.GlobalConstants.WEB_URL;
 import static testHelpers.StringValues.*;
 
 public class RegistrationTests {
+
+    private static Logger logger = Logger.getLogger(RegistrationTests.class);
+
+    @Rule
+    public TestRule watcher = new TestWatcher() {
+        protected void starting(Description description) {
+            logger.info("Starting test: " + description.getMethodName());
+        }
+    };
 
     @Test
     public void accountRegistration() throws IllegalAccessException {
